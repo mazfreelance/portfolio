c4778b5de6d836cbab0b883cf99e5a7353be9cf2
mazwebfreelance <maz.webfreelance@gmail.com>
2022-08-22 05:20:09 +0800

    update logo

--- components/layouts/shared/meta.tsx
+++ components/layouts/shared/meta.tsx
@@ -10,13 +10,13 @@ const Meta = (props: MetaProps): JSX.Element => {
             <meta name="keywords" content={props.keywords} />
             <meta name="description" content={props.description} />
             <meta property="og:title" content={props.title} />
-            <meta property="og:image" content={props.image ? props.image : '/logo.png'} />
+            <meta property="og:image" content={props.image ? props.image : '/logo-ma.png'} />
             <meta property="og:description" content={props.description} />
             <meta property="og:site_name" content={props.title} />
             <title>
                 {props.title.includes('Azmin') ? props.title : props.title.concat(' | Mohd Azmin')}
             </title>
-            <link rel="icon" type="image/x-icon" href="/favicon-48x48.ico" />
+            <link rel="icon" type="image/x-icon" href="/favicon-ma.ico" />
         </Head>
     )
 }
@@ -26,7 +26,7 @@ Meta.defaultProps = {
     'title': 'Mohd Azmin | Full Stack Developer',
     'description': 'I am Full Stack Developer. Love to find new technology and knowledge',
     'keywords': 'mohdazmin, mohd azmin, web developer, full stack developer, backend developer',
-    'image': 'https://mohdazmin.com/logo.png'
+    'image': 'https://mohdazmin.com/logo-ma.png'
 }
 
 export default Meta
